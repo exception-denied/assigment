8b9cdca375dae961aedb84d6bcee6b5c24840ee5
exception-denied <krishna.201360046@nist.edu>
2020-08-13 22:14:32 +0530

    update user data

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,16 +1,20 @@
+
+locals {
+  instance-userdata = <<EOF
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+EOF
+}
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name = var.ec2_key
   subnet_id = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
-  user_data = << EOF
-		      #! /bin/bash
-          sudo apt-get -y update
-          sudo apt-get -y install nginx
-          sudo service nginx start
-          echo "<h1>Deployed via Terraform</h1>" >> sudo tee /var/www/html/index.html
-  EOF
+  user_data_base64 = "${base64encode(local.instance-userdata)}"
 }
 
 resource "aws_elb" "web" {
